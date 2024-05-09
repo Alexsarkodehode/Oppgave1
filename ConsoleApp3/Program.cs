fccad248fcdbdaeecd07b18c2f8952ce209cf113
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2024-05-10 01:33:10 +0200

    ok

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Hello, World!");
+            ConsoleApp3.ReadLine();
 
             
         }
